10bc7066c4947d697197197f74c5788e0f1c326b
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2014-03-06 22:40:47 +0900

    singleton化

--- lib/Acme/LoveLive/Person.pm
+++ lib/Acme/LoveLive/Person.pm
@@ -32,6 +32,21 @@ sub call { '' }
         fullname_ja first_name_en last_name_en call cv
         irc_color cyalume_rgb image_rgb
     );
+
+    my $_instance;
+    sub instance {
+        my ($class) = @_;
+        my $package = __PACKAGE__;
+        die 'abstract method' if $package eq $class;
+
+        (my $fullname = $class) =~ s/${package}:://;
+        return $_instance->{$fullname} if exists $_instance->{$fullname};
+
+        my %args = map { my $attr = $_; "__$attr" => $data->{person}->{$fullname}->{$attr}; } @attributes;
+        return $_instance->{$fullname} = bless \%args, $class;
+    }
+    *new = \&instance;
+
     for my $fullname (keys %{ $data->{person} }) {
         no strict 'refs';
 
@@ -39,16 +54,9 @@ sub call { '' }
         @{"${class}::ISA"} = qw(
             Acme::LoveLive::Person
         );
-
         for my $attribute (@attributes) {
             *{"${class}::${attribute}"} = sub { shift->{"__$attribute"} };
         }
-
-        *{"${class}::new"} = sub {
-            my ($klass) = @_;
-            my %args = map { my $attr = $_; "__$attr" => $data->{person}->{$fullname}->{$attr}; } @attributes;
-            return bless \%args, $klass;
-        };
     }
 }
 
